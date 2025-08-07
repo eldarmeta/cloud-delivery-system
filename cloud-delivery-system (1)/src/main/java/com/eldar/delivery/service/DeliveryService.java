2aad789c21acc4393c77159784cf4e44d7d5b8a7
Eldar Abdivaitov <eldarmeta@outlook.com>
2025-08-06 23:52:38 -0400

    Create DeliveryService.java

---
+++ cloud-delivery-system (1)/src/main/java/com/eldar/delivery/service/DeliveryService.java
@@ -0,0 +1,23 @@
+package com.eldar.delivery.service;
+
+import com.eldar.delivery.model.Delivery;
+import com.eldar.delivery.repository.DeliveryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DeliveryService {
+
+    @Autowired
+    private DeliveryRepository deliveryRepository;
+
+    public Delivery createDelivery(Delivery delivery) {
+        return deliveryRepository.save(delivery);
+    }
+
+    public List<Delivery> getAllDeliveries() {
+        return deliveryRepository.findAll();
+    }
+}
